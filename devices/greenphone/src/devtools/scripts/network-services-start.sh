448594514c4b3c99c015b1e086efa0e2997ead1f
Neil Jerram <neil@ossau.homelinux.net>
2012-12-16 11:14:44 +0000

    Remove devices/greenphone

--- devices/greenphone/src/devtools/scripts/network-services-start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -z "$( pidof inetd )" ]; then
-    /usr/sbin/inetd
-fi
-
-# notify Qtopia that some new network devices may have appeared 
-qcop send "QPE/NetworkState" "updateNetwork()"
-
